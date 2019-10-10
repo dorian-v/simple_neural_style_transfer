6ebe0180e23fe8fc1aeee6066d44c093e6898e7e
dorian <dorian.voravong@epitech.eu>
2019-10-10 17:47:14 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# simple_neural_style_transfer
+# Neural Style Transfer
+
+TensorFlow implementation of "A Neural Algorithm of Artistic Style" by 
+Leon A. Gatys, Alexander S. Ecker, Matthias Bethge (https://arxiv.org/abs/1508.06576).
+
+Based on Course 4 Week 4 practical exercise of deeplearning.ai's Deep Learning Specialization
+on Coursera.
+
+
+Using a pre-trained convolutional neural network, we combine the content of an image and
+the style of another to generate a new image.
+
+## Dependencies
+
+Numpy
+
+Scipy
+
+TensorFlow
+
+
+## Usage
+
+Step1: Install dependencies
+
+Step2: Download pre-trained VGG-19 model (available at http://www.vlfeat.org/matconvnet/pretrained/#imagenet-ilsvrc-classification),
+and place it in a "/pretrained-model" directory
+
+Step3:
+```
+python run.py images/content_image.jpg images/style_image.jpg
+```
+
+Step4: Look at the generated images in the "output" directory
+
+## Examples
+
+![Example1](./images/golden_gate.jpg)
+![Example2](./output/golden_gate_picasso/generated_image.jpg)
+![Example3](./output/golden_gate_starry_night/generated_image.jpg)
+![Example4](./output/golden_gate_scream/generated_image.jpg)
+![Example5](./output/golden_gate_monet/540.png)
---
+++ run.py
@@ -0,0 +1,207 @@
+import tensorflow as tf
+import sys
+import scipy.misc
+from vgg_model import load_vgg_model
+from image_tools import *
+
+STYLE_LAYERS = [
+    ('conv1_2', 0.25),
+    ('conv2_2', 0.25),
+    ('conv3_2', 0.25),
+    ('conv4_2', 0.25)]
+
+alpha = 1
+beta = 10
+lambd = 10 ** 4
+gamma = 10 ** -3
+nu = 1
+
+
+def get_content_image_activations(model, content_image):
+    # Assign the content image to be the input of the VGG model.
+    sess.run(model['input'].assign(content_image))
+
+    # Select the output tensor of layer conv4_2
+    out = model['conv4_2']
+
+    # Set a_C to be the hidden layer activation from the layer we have selected with content image as input
+    a_C = sess.run(out)
+    return a_C, out
+
+
+def compute_content_cost(a_C, a_G):
+    """
+    Computes the content cost
+
+    Arguments:
+    a_C -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image C
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image G
+
+    Returns:
+    J_content -- scalar that you compute using equation 1 above.
+    """
+
+    # Retrieve dimensions from a_G
+    m, n_H, n_W, n_C = a_G.get_shape().as_list()
+
+    # Reshape a_C and a_G
+    a_C_unrolled = tf.transpose(tf.reshape(a_C, [n_H * n_W, n_C]))
+    a_G_unrolled = tf.transpose(tf.reshape(a_G, [n_H * n_W, n_C]))
+
+    # compute the cost with tensorflow
+    J_content = tf.reduce_sum(tf.square(tf.subtract(a_C_unrolled, a_G_unrolled))) / (4 * n_H * n_W * n_C)
+
+    return J_content
+
+
+def gram_matrix(A):
+    """
+    Argument:
+    A -- matrix of shape (n_C, n_H*n_W)
+
+    Returns:
+    GA -- Gram matrix of A, of shape (n_C, n_C)
+    """
+
+    GA = tf.matmul(A, tf.transpose(A))
+
+    return GA
+
+def get_style_image_activations(model, STYLE_LAYERS, style_image):
+
+    # Assign the input of the model to be the "style" image
+    sess.run(model['input'].assign(style_image))
+
+    a_S = {}
+    for layer_name, _ in STYLE_LAYERS:
+        out = model[layer_name]
+        a_S[layer_name] = sess.run(out)
+
+    return a_S
+
+def compute_layer_style_cost(a_S, a_G):
+    """
+    Arguments:
+    a_S -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image S
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image G
+
+    Returns:
+    J_style_layer -- tensor representing a scalar value, style cost defined above by equation (2)
+    """
+
+    # Retrieve dimensions from a_G (≈1 line)
+    m, n_H, n_W, n_C = a_G.get_shape().as_list()
+
+    # Reshape the images to have them of shape (n_C, n_H*n_W) (≈2 lines)
+    a_S = tf.transpose(tf.reshape(a_S, [n_H * n_W, n_C]))
+    a_G = tf.transpose(tf.reshape(a_G, [n_H * n_W, n_C]))
+
+    # Computing gram_matrices for both images S and G (≈2 lines)
+    GS = gram_matrix(a_S)
+    GG = gram_matrix(a_G)
+
+    # Computing the loss (≈1 line)
+    J_style_layer = tf.reduce_sum(tf.square(tf.subtract(GS, GG))) / (4 * n_C **2 * (n_W * n_H) ** 2)
+
+    return J_style_layer
+
+
+def compute_style_cost(model, STYLE_LAYERS, a_S):
+    # initialize the overall style cost
+    J_style = 0
+
+    for layer_name, coeff in STYLE_LAYERS:
+        # Select the output tensor of the currently selected layer
+        out = model[layer_name]
+
+       # Will be evaluated later with session run
+        a_G = out
+
+        # Add weighted style cost of layer to overall style cost
+        J_style += coeff * compute_layer_style_cost(a_S[layer_name], a_G)
+
+    return J_style
+
+
+def compute_total_variation_regularization(image):
+    return tf.image.total_variation(image)[0]
+
+
+def total_cost(J_content, J_style, J_TV, alpha=10, beta=40, gamma=10**-3):
+    J = alpha * J_content + beta * J_style + gamma * J_TV
+    return J
+
+
+def define_total_cost_function(model, STYLE_LAYERS, a_C, a_S, a_G):
+    J_content = compute_content_cost(a_C, a_G)
+    J_style = compute_style_cost(model, STYLE_LAYERS, a_S)
+    J_TV = compute_total_variation_regularization(model['input'])
+    J = total_cost(J_content, J_style, J_TV, alpha, beta, gamma)
+    return J_content, J_style, J_TV, J
+
+
+def model_nn(sess, input_image, num_iterations=200):
+
+    sess.run(tf.compat.v1.global_variables_initializer())
+    # Run the noisy input image (initial generated image) through the model
+    sess.run(model['input'].assign(input_image))
+
+    for i in range(num_iterations):
+
+        # Run the session on the train_step to minimize the total cost
+        _ = sess.run(train_step)
+
+        # Compute the generated image by running the session on the current model['input']
+        generated_image = sess.run(model['input'])
+
+        # Save generated image every 20 iteration.
+        if i % 20 == 0:
+            Jt, Jc, Js, Jtv = sess.run([J, J_content, J_style, J_TV])
+            print("Iteration " + str(i) + " :")
+            print("total cost = " + str(Jt))
+            print("content cost = " + str(Jc))
+            print("style cost = " + str(Js))
+            print('total variation cost = ' + str(Jtv))
+            # save current generated image in the "/output" directory
+            save_image("output/" + str(i) + ".png", generated_image)
+
+
+    # save last generated image
+    save_image('output/generated_image.jpg', generated_image)
+
+    return generated_image
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print('Usage: python ' + sys.argv[0] + ' content_image_path style_image_path')
+        exit(1)
+
+    tf.compat.v1.reset_default_graph()
+    sess = tf.compat.v1.InteractiveSession()
+
+    # Load and prepare content and style images
+    content_image = scipy.misc.imread(sys.argv[1])
+    content_image = reshape_and_normalize_image(content_image)
+    style_image = scipy.misc.imread(sys.argv[2])
+    style_image = reshape_and_normalize_image(style_image)
+
+    # Initialize output image
+    generated_image = generate_noise_image(content_image)
+
+    # Load pre-trained VGG19 model
+    model = load_vgg_model("pretrained-model/imagenet-vgg-verydeep-19.mat")
+
+    # Set a_C to be the hidden layer activation from the layer we have selected with content image as input
+    # Set a_G to be the activations from the same layer, but not yet evaluated
+    a_C, a_G = get_content_image_activations(model, content_image)
+
+    # Set a_S to be the hidden layers activations from the layers we have selected with style image as input
+    a_S = get_style_image_activations(model, STYLE_LAYERS, style_image)
+
+    # define cost function, optimizer and training steps
+    J_content, J_style, J_TV, J = define_total_cost_function(model, STYLE_LAYERS, a_C, a_S, a_G)
+    optimizer = tf.compat.v1.train.AdamOptimizer(2.0)
+    train_step = optimizer.minimize(J)
+    model_nn(sess, generated_image, 1000)
